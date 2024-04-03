23a7c0c63f31639f6829a2e81478130ca5189e04
xClune <aclune382@gmail.com>
2024-04-03 16:19:28 +0800

    Create function for tracking cell value.
    Add methods within controller for affecting value and switching player.
    
    Add game text displaying active player.

--- script.js
+++ script.js
@@ -1,59 +1,106 @@
-// Create the grid:
-createBoard(3, 3);
-
-function createBoard(rows, columns){
+// Create the board:
+function createBoard(){
         const gridContainer = document.querySelector('.grid-container');
 
+        const rows = 3;
+        const columns = 3;
+
         let board = [];
-        let turn = 'X';
 
         // Generate Cells
         for (let i = 0; i < rows; i++){
             for (let j = 0; j < columns; j++){
+                // Create grid on page
                 let cell = document.createElement('div');
                 cell.classList.add('cell');
                 gridContainer.appendChild(cell);
-                board.push(0);
+
+                // Create actual board state
+                board.push(Cell());
             }
         }
 
         // Event listeners for click
         let squares = document.querySelectorAll('.cell');
-        for (const square of squares) {
+        for (const [index, square] of squares.entries()) {
             square.addEventListener('click', () => {
                 if (!square.classList.contains('selected')){
                     square.classList.toggle('selected');
-                    playTurn();
+                    square.innerText = `${GameController().getActivePlayer()}`
+                    GameController().playRound(board[index])
                 }
             })
         }
-    }
 
-// Switch turn
-function switchTurn(playerTurn) {
-    if (playerTurn == 'X'){
-        playerTurn = 'O';
-    } else {
-        playerTurn = 'X';
+        // method to retrieve board
+        const getBoard = () => board;
+
+        const checkSquare = (square, player) => {
+            if (square.getValue() === 0){
+                square.selectSquare(player);
+            }
+        }
+
+    return {
+        getBoard,
+        checkSquare
     }
+}
+
+function Cell() {
+    let value = 0;
+
+    // create method for changing square value onclick
+    const selectSquare = (player) => {
+        value = player;
+    };
+
+    // method for retreiving value of square
+    const getValue = () => value;
+
+    return {
+        selectSquare,
+        getValue
+    };
 }
 
 // Game controller
+function GameController() {
+    const board = createBoard();
+    let gameText = document.querySelector('.game-text');
+  
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === 'X' ? 'O' : 'X';
+    };
+    const getActivePlayer = () => activePlayer;
+  
+    const printNewRound = () => {
+      gameText.innerText = `${activePlayer}'s turn.`;
+    };
+  
+    const playRound = (square) => {
 
-function playTurn() {
-    // get board state
-    let board = []
-
-    let squares = document.querySelectorAll('.cell');
-    for (const square of squares) {
-        if (!square.classList.contains('selected')){
-            board.push(0);
-        } else {
-            board.push(1);
-        }
-    }
-    console.log(board);
+        // change value of square to player token
+        board.checkSquare(square, activePlayer);
+    
+        // Switch player turn
+        switchPlayerTurn();
+        printNewRound();
+    };
+    // Initial play game message
+    printNewRound();
+  
+    // For the console version, we will only use playRound, but we will need
+    // getActivePlayer for the UI version, so I'm revealing it now
+    return {
+      playRound,
+      getActivePlayer
+    };
 }
+
+var activePlayer = 'X';
+const game = GameController();
+  
 // Determine winner
 
 
